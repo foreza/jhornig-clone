b81347eb7c3497aba553249eec8132ff3413b41c
Jason Chiu <jforeza@gmail.com>
2020-01-16 19:58:17 -0800

    Staging temporarily

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+
+// Slider implementation
+var imageSourceArray = [
+    "../images/JHornig-Caffe-Crema-Classico.jpg",
+    "../images/JHornig-Caffe-Crema-Classico.jpg",
+    "../images/JHornig-Caffe-Crema-Classico.jpg"
+    "../images/JHornig-Caffe-Crema-Classico.jpg",
+    "../images/JHornig-Caffe-Crema-Classico.jpg",
+    "../images/JHornig-Caffe-Crema-Classico.jpg",
+    "../images/JHornig-Caffe-Crema-Classico.jpg",
+    "../images/JHornig-Caffe-Crema-Classico.jpg",
+    "../images/JHornig-Caffe-Crema-Classico.jpg"
+]
+
+$(document).ready(function () {
+
+
+
+});
+
+
+/*
+
+1 - perform an animation to move all DOM elements to the left a certain amount X
+2 - take the furthest left Y elements and pop them 
+3 - as you pop an element, push it to the back of the array
+
+Should I be manipulating the entire row?? 
+
+
+*/
+function slideLeft(){
+
+
+
+}
+
+
